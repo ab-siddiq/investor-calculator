52d6f81d02e2bf33e2b5c67adb036d6d925c019d
siddiq <siddiq.aiub@gmail.com>
2021-02-12 23:09:01 +0600

    added functionality

--- main.js
+++ main.js
@@ -27,4 +27,75 @@ window.onload = function () {
     };
     $("#chartContainer").CanvasJSChart(options);
     
+}
+
+let investorForm = document.getElementById("investor-form");
+investorForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+function investorInput() {
+    let investorRiskInput = getSelectInputValue("investor-risk-tolerance-input");
+    let investorCurrentAssetInput = getInputFieldValue("investor-current-asset-input");
+    let investorSavingPerYearInput = getInputFieldValue("investor-saving-per-year-input");
+    let investorMarginalTaxRateInput = parseFloat(getSelectInputValue("investor-marginal-tax-rate-input"));
+    let investorRequiredIncomeNextTwoYearInput = getInputFieldValue("investor-required-income-next-two-year-input");
+    
+    // console.log(investorRequiredIncomeNextTwoYearInput, typeof (investorRequiredIncomeNextTwoYearInput));
+    // console.log(parseFloat(investorMarginalTaxRateInput), typeof (parseFloat(investorMarginalTaxRateInput)));
+   
+    // console.log(investorMarginalTaxRateInput, typeof (investorMarginalTaxRateInput));
+    
+    // console.log(investorRiskInput,investorCurrentAssetInput,investorSavingPerYearInput,investorMarginalTaxRateInput,investorRequiredIncomeNextTwoYearInput);
+    
+    let ekushFund = calculateInvestorInput(investorRiskInput, investorCurrentAssetInput);
+    investorCalculationFormula(ekushFund,investorCurrentAssetInput, investorSavingPerYearInput, investorMarginalTaxRateInput, investorRequiredIncomeNextTwoYearInput);
+}
+
+function getInputFieldValue(id) {
+    let inputFieldValue = document.getElementById(id).value;
+    let inputFieldValueNumber = parseFloat(inputFieldValue);
+    return inputFieldValueNumber;
+}
+
+function getSelectInputValue(id) {
+    let selectOptionValue = document.getElementById(id);
+    let selectInputValue = selectOptionValue.options[selectOptionValue.selectedIndex].text;
+    return selectInputValue;
+}
+
+function calculateInvestorInput(risk,asset) {
+    if (risk == "Low" && asset <= 5000000) {
+        return 0;
+    } else if (risk == "Moderate" && asset <= 5000000) {
+        return 10/100;
     }
+     else if (risk == "High" && asset <= 5000000) {
+        return 20/100;
+    }
+     else if (risk == "Low" && asset > 5000000) {
+        return 30/100;
+    }
+     else if (risk == "Moderate" && asset > 5000000) {
+        return 40/100;
+    }
+     else if (risk == "High" && asset > 5000000) {
+        return 50/100;
+    }
+}
+
+function investorCalculationFormula(ekusFund,asset,savings,marginalTaxRate,incomeRequired) {
+    let liquidityConstraint = (savings / 12) * 6;
+    let cash = liquidityConstraint / asset;
+    let bankFDR = incomeRequired / 100;
+    let shanchaypatra = 100 - ekusFund - bankFDR - cash;
+
+    showInvestorOutput(ekusFund,liquidityConstraint,cash,bankFDR,shanchaypatra);
+}
+
+function showInvestorOutput(ekusFund,liquidityConstraint,cash,bankFDR,shanchaypatra) {
+    document.getElementById("ekush-liquidity-constraint").innerHTML = liquidityConstraint;
+    document.getElementById("ekush-fund").innerHTML = ekusFund;
+    document.getElementById("ekush-shanchaypatra").innerHTML = shanchaypatra;
+    document.getElementById("ekush-bank-fdr").innerHTML = bankFDR;
+    document.getElementById("ekush-cash").innerHTML = cash;
+}
